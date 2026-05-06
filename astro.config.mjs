74779c81aec54e9f762021fd07b34ecc504287e6
Janvier Byiringiro <jenzzly@gmail.com>
2026-05-06 14:26:38 +0200

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://jenzzly.github.io',
-  base: '/rwandavisa.com/',
+  base: '',
   output: 'static',
   trailingSlash: 'always'
 });
